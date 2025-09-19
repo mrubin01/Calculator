52e3928db4e29ecac7121f8243557eec41a77c94
mrubin01 <mi.rubino@protonmail.com>
2025-09-19 14:29:51 +0100

    Add operation functions

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ labels.forEach(label => {
     button.textContent = label;
     container.appendChild(button);
 })
+
+function add(n1, n2) {
+    return n1 + n2;
+}
+
+function subtract(n1, n2) {
+    return n1 - n2;
+}
+
+function multiply(n1, n2) {
+    return n1 * n2;
+}
+
+function divide(n1, n2) {
+    return n1 / n2;
+}
