a0f1f736e82839f07dbfb225c077e2b060f62ed8
mrubin01 <mi.rubino@protonmail.com>
2025-09-20 08:48:28 +0100

    Implement function operate

--- script.js
+++ script.js
@@ -31,3 +31,15 @@ function multiply(n1, n2) {
 function divide(n1, n2) {
     return n1 / n2;
 }
+
+function operate(n1, n2, operator) {
+    if (operator === "+") {
+        add(n1, n2); 
+    } else if (operator === "-") {
+        subtract(n1, n2);
+    } else if (operator === "*") {
+        multiply(n1, n2);
+    } else if (operator === "/") {
+        divide(n1, n2);
+    }
+}
